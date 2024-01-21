f33389c35b55aed06e07c27f5a53da96ca5e135b
Habib <129363789+habibqureshi488@users.noreply.github.com>
2024-01-21 09:45:43 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SearchParams from "./components/Param";
+function App(){
+    return(
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    )
+}
+
+export default App;
